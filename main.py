2e2e2cc632ff343841edf0d4b0e8a526ec1d3477
Rodriguez Lautaro <lautaro.rodriguez553@gmail.com>
2022-01-21 10:54:43 -0300

    Se agrego toda la logica de reenviar mensajes

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from lib2to3.pgen2 import token
-from telegram.ext import Updater, CommandHandler
+
+
+from telegram.ext import Updater, MessageHandler,Filters
 
 def start(update,context):
     update.message.reply_text("Hola pibe")
 
-if __name__ == "__main__":
-    updater = Updater(token="YOUR_TOKEN", use_context = True)
+def process_message(update,context):
 
+    text = update.message.text
+    id_origin_chat= update.message.chat.id
+
+    if str(id_origin_chat) == "-688891110":
+        context.bot.send_message(
+        chat_id = "-626626137",
+        text =  text
+        )
+
+
+def run():
+    updater = Updater(token="5076850110:AAHLAvi4scMP4RwZ6W5TOsEYENebjSNHSsw", use_context = True)
+
+    
     dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(MessageHandler(filters=Filters.all ,callback=process_message))
 
     updater.start_polling()
+    print("Bot is polling")
     updater.idle()
+
+
+if __name__ == "__main__":
+    run()
